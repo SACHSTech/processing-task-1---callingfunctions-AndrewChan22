b5c3923e33096fce095fabe0d73a177c33888b11
Andrew <andrew.chan23@ycdsbk12.ca>
2022-02-24 09:55:54 -0500

    Completed 5.2 Processing Task

--- Sketch.java
+++ Sketch.java
@@ -22,7 +22,7 @@ public class Sketch extends PApplet {
   public void settings() {
 	// put your size call here
       //()
-    size(800, 800);
+    size(400, 400);
   }
 
   /** 
@@ -38,10 +38,10 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-    //draw ground
+    // draw ground
 
     noStroke();
-
+    
     fill(113, 81, 55);
     rect(0, 650, 800, 200);
     grass(0, 650);
@@ -57,7 +57,7 @@ public class Sketch extends PApplet {
     stroke(0, 0, 0);
     strokeWeight(1);
 
-    //draw sun 
+    // draw sun 
     fill(255, 255, 35);
     ellipse(100, 100, 75, 75);
 
@@ -66,36 +66,35 @@ public class Sketch extends PApplet {
 
     line(127, 127, 160, 160);
 
-    //draw house
+    // draw house
     fill(168, 97, 87);
     rect(225, 325, 350, 325);
 
-    //draw roof
+    // draw roof
     fill(124, 117, 110);
     triangle(400, 175, 165, 325, 635, 325);
 
-    //draw left window
+    // draw left window
     fill(122, 184, 221);
     ellipse(313, 420, 50, 75);
     line(288, 420, 338, 420);
     line(313, 383, 313, 457);
 
-    //draw right window
+    // draw right window
     ellipse(488, 420, 50, 75);
     line(464, 420, 513, 420);
     line(488, 383, 488, 456);
 
-    //draw door
+    // draw door
     fill(123, 68, 29);
     rect(350, 500, 100, 150);
 
-    //draw door knob
+    // draw door knob
     fill(70, 71, 77);
     ellipse(375, 565, 15, 15);
 
+    // draw clouds
     noStroke();
-
-    //draw clouds
     fill(255, 255, 255);
     ellipse(300, 50, 100, 50);
     ellipse(350, 70, 100, 50);
@@ -111,8 +110,18 @@ public class Sketch extends PApplet {
 
   
   // define other methods down here.
-  public void grass(float x, float y) {
+  
 
+  
+  /**
+   *  Given two floats, return the x and y coordinates of a rectangle
+   * 
+   * @param x The first x coordinate
+   * @param y The first y coordinate
+   * @return the two floats as the x and y coordinate of a rectangle
+   * @author A. Chan
+   */
+    public void grass(float x, float y) {
 
     fill(147, 192, 97);
     rect(x, y, 50, 75);
